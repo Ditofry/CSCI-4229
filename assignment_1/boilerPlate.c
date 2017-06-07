4c1dced1ffc5aeb9f5806a47821deee275ff2f28
Ditofry <bbarrett@victorops.com>
2017-06-07 00:20:00 -0600

    assignment 0 completed

---
+++ assignment_1/boilerPlate.c
@@ -0,0 +1,42 @@
+/*
+ * Brandon's openGL BoilerPlate, which is basically just ex1 from the graphics
+ * class...
+ */
+
+// Submitting for grades on a Linux machine
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+void display() {
+   //  Clear screen
+   glClear(GL_COLOR_BUFFER_BIT);
+   
+   //  Draw stuff
+   glBegin(GL_POLYGON);
+   glVertex2f( 0.0, 0.5);
+   glVertex2f( 0.5,-0.5);
+   glVertex2f(-0.5,-0.5);
+   glEnd();
+   
+   //  Make scene visible
+   glFlush();
+}
+
+/*
+ * GLUT based Hello World
+ */
+int main(int argc,char* argv[]) {
+   //  Initialize GLUT
+   glutInit(&argc,argv);
+   //  Create window
+   glutCreateWindow("Hello World");
+   //  Register function used to display scene
+   glutDisplayFunc(display);
+   //  Pass control to GLUT for events
+   glutMainLoop();
+   //  Return to OS
+   return 0;
+}
